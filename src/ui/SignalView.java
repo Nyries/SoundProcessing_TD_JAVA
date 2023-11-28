6a0469f06f9ad72682fc11595685d46be316643e
Leoap <leo.appourchaux@gmail.com>
2023-11-28 10:41:44 +0100

    SignalView

--- src/ui/SignalView.java
+++ src/ui/SignalView.java
@@ -1,4 +1,22 @@
 package ui;
 
-public class SignalView {
+import audio.AudioSignal;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+
+public class SignalView extends LineChart<Number, Number> {
+
+    private XYChart.Series<Number, Number> series;
+    private AudioSignal audiosignal;
+
+    public SignalView(NumberAxis x, NumberAxis y) {
+        super(x, y);
+        series = new XYChart.Series<>();
+        getData().add(series);
+    }
+
+    public void setAudiosignal(AudioSignal audiosignal) {
+        this.audiosignal = audiosignal;
+    }
 }
